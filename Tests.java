bd4cae3d7cedc456dee13457104e2d4467824952
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-02-11 13:28:49 -0700

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -5,6 +5,7 @@ public class Tests {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("solid dog");
+		System.out.println("lola wuz here");
 	}
 
 }
