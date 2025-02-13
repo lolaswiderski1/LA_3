84da63fcc91617479fe62c54174ca423f756e24c
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-02-13 14:13:08 -0700

    Update SongTest.java

--- SongTest.java
+++ SongTest.java
@@ -7,8 +7,18 @@ import org.junit.jupiter.api.Test;
 class SongTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetTitle() {
+		Song song = new Song("long time", "playboi carti");
+		assertEquals("long time", song.getTitle());
 	}
-
+	@Test
+	void TestGetArtist() {
+		Song song = new Song("long time", "playboi carti");
+		assertEquals("playboi carti", song.getArtist());
+	}
+	@Test
+	void testToString() {
+		Song song = new Song("long time", "playboi carti");
+		assertEquals("long time, playboi carti", song.toString());
+	}
 }
