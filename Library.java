cc4372dc584b52e2a496f3c18abd6e5890aba8c2
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-02-17 07:44:35 -0700

    Update Library.java

--- Library.java
+++ Library.java
@@ -3,22 +3,22 @@ package Music;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Library<T> {
+public class Library<Object> {
     private MusicStore store;
-    private ArrayList<T> library;
+    private ArrayList<Object> library;
 
     public Library() {
         store = new MusicStore();
-        library = new ArrayList<T>();
+        library = new ArrayList<Object>();
     }
 
     public void addPlayList(String name) {
         PlayList playList = new PlayList(name);
-        library.add((T) playList);
+        library.add( (Object) playList);
     }
 
     public List<Song> searchPlayListByName(String name) {
-        for (T items : library) {
+        for (Object items : library) {
             if (items instanceof PlayList) {
                 PlayList playList = (PlayList) items;
                 if (name.equals(playList.getName())) {
@@ -32,7 +32,7 @@ public class Library<T> {
     public void addSong(Song song) {
     	// if song exists in musicStore
     	if (store.getSongByTitle(song.getTitle())[0] == song.getTitle()) {
-    		library.add((T) song);
+    		library.add((Object) song);
         }
     }
 }
