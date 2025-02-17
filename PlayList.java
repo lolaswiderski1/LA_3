5736ad43bf651f95d7acb1c7f116e6a8f8a7b331
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-02-16 18:14:32 -0700

    Add files via upload
    Playlist class

---
+++ PlayList.java
@@ -0,0 +1,20 @@
+package Music;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PlayList {
+	private String name;
+	private Map<String, Song> songs = new LinkedHashMap<>();
+	
+	public void PlayList(String name) {
+		this.name = name;
+	}
+	public void addSong(Song song) {
+		songs.put(song.getTitle(), song);
+	}
+	public void removeSong(Song song) {
+	    songs.remove(song.getTitle()); 
+	}
+}
