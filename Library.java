39379186b85b62c5d7a6ebb8dfeaa1370dc7f8d2
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-02-27 12:24:23 -0700

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,38 +1,126 @@
 package Music;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class Library<Object> {
-    private MusicStore store;
-    private ArrayList<Object> library;
+class LibraryModel{
+   
+	private MusicStore store;
+    private HashMap<Song, Rating> songs;
+    private ArrayList<PlayList> playLists;
+    private ArrayList<Album> albums;
+    private ArrayList<Song> favorites;
+    
+    public enum Rating {
+    	ONE, TWO, THREE, FOUR, FIVE;
+    }
 
-    public Library() {
+    public LibraryModel() {
         store = new MusicStore();
-        library = new ArrayList<Object>();
+        songs = new HashMap<Song, Rating>();
+        favorites = new ArrayList<Song>();
+        albums = new ArrayList<Album>();  
     }
-
-    public void addPlayList(String name) {
-        PlayList playList = new PlayList(name);
-        library.add( (Object) playList);
-    }
-
-    public List<Song> searchPlayListByName(String name) {
-        for (Object items : library) {
-            if (items instanceof PlayList) {
-                PlayList playList = (PlayList) items;
-                if (name.equals(playList.getName())) {
-                    return playList.getSongs();
-                }
-            }
-        }
-        return null;
-    }
-
+    
     public void addSong(Song song) {
-    	// if song exists in musicStore
-    	if (store.getSongByTitle(song.getTitle())[0] == song.getTitle()) {
-    		library.add((Object) song);
-        }
+    	
+    	for (Album album : store.getAlbums()) {
+    		Object[] albumSongs = album.getAllSongs();
+    		ArrayList<Song> songsList = (ArrayList<Song>) albumSongs[1];
+    		for (Song song1: songsList) {
+    			if (song.getArtist().equals(song1.getArtist()) && 
+    					song.getTitle().equals(song1.getTitle()))  {
+    				songs.put(song, null);
+    			}
+    		}
+    	}
     }
+    
+    public void addAlbum(Album album) {
+    	
+    	for (Album album1 : store.getAlbums()) {
+    		if (album1.getArtist().equals(album.getArtist()) && 
+    				album1.getTitle().equals(album.getTitle()) && 
+    				album1.getGenre().equals(album.getGenre()) &&
+    				album1.getYear().equals(album.getYear())) {
+    			albums.add(album);
+    			
+    		}
+    	}
+    }
+    
+    public void addPlaylist(PlayList pl) {
+    	playLists.add(pl);
+    }
+    
+    public void rateSong(Rating rating, Song song) {
+    	
+    	for (Song song1 : songs.keySet()) {
+    		if (song1.getTitle().equals(song.getTitle()) &&
+    				song1.getArtist().equals(song.getArtist())) {
+    			songs.put(song, rating);
+    			
+    			if (rating == Rating.FIVE) {	// if rating 5 add to favorites
+        			favorites.add(song);
+    			}
+    		}
+    	}
+    }
+    
+    public ArrayList<String> getSongTitles() {
+    	
+    	ArrayList<String> titles = new ArrayList<String>();
+    	for (Song song : songs.keySet()) {
+    		titles.add(song.getTitle());
+    	}
+    	return titles;
+    }
+    
+    public ArrayList<String> getSongArtists() {
+    	
+    	ArrayList<String> artists = new ArrayList<String>();
+    	for (Song song : songs.keySet()) {
+    		artists.add(song.getArtist());
+    	}
+    	return artists;
+    }
+    
+    public ArrayList<Album> getAlbums() {
+    	return new ArrayList<>(albums);
+    }
+    
+    public ArrayList<PlayList> getPlayLists() {
+    	return new ArrayList<>(playLists);
+    }
+    
+    public ArrayList<Song> getFavorites() {
+    	return new ArrayList<>(favorites);
+    }
+    
+    public List<Song> getSongsByTitle(String title) {
+    	return store.getSongsByTitle(title);
+    }
+    
+    public List<Song> getSongsByArtist(String artist) { 
+    	return store.getSongsbyArtist(artist);
+    }
+   
+    public Album getAlbumByTitle(String title) {
+    	return store.getAlbumByTitle(title);
+    }
+    
+    public List<Album> getAlbumsByArtist(String artist) {
+    	return store.getAlbumsByArtist(artist);
+    }
+    
+    public ArrayList<Song> getPlayList(PlayList playList) {
+    	ArrayList<Song> pLSongs = new ArrayList<Song>();
+    	for (PlayList playList1: playLists) {
+    		if (playList1.getName().equals(playList.getName())) {
+    			return new ArrayList<>(playList1.getSongs());
+    		}
+    	} return null;
+    }
+    
 }
