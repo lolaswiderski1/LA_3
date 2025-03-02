941634f00d4daf64ac14b665ca3692626ca09148
lolaswiderski1 <lolaswiderski22@gmail.com>
2025-03-01 20:00:09 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,28 +1,38 @@
-package Model;
+// Sam Hershey, Lola Swiderski
+// class to model a song object
 
+package LA1;
+
+// songs are immutable
 public final class Song {
+	// instantiate title, artist, and album title
 	private String songTitle;
 	private String artist;
 	private String albumTitle;
 	
+	// initialize variables
 	public Song(String songTitle, String artist, String albumTitle) {
 		this.songTitle = songTitle;
 		this.artist = artist;	
 		this.albumTitle = albumTitle;
 	}
 	
+	// get song title
 	public String getSongTitle() {
 		return songTitle;
 	}
 	
+	// get song artist
 	public String getArtist() {
 		return artist;
 	}
 	
+	// get album title
 	public String getAlbumTitle() {
 		return albumTitle;
 	}
 	
+	// print song
 	public String toString() {
 		return songTitle + ", " + artist;
 	}
