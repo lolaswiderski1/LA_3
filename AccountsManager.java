fc245a483c848a0cab99cab49dc216676a15a3e8
SamHershey <134254681+SamHershey@users.noreply.github.com>
2025-03-24 20:32:08 -0700

    Update AccountsManager.java

--- AccountsManager.java
+++ AccountsManager.java
@@ -1,12 +1,12 @@
+// Authors: Sam Hershey, Lola Swiderski
+// Description:	Used to create and access user accounts. Adds new usernames
+// and associated passwords to a database.
 package userManagement;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import model.LibraryModel;
 
 public class AccountsManager {
-	private String dataDirectory;
+	private String dataDirectory; // name of folder containing all json files
 	private UserDatabaseManager dbManager;
 	
 	public AccountsManager(String dataDirectory) {
@@ -14,27 +14,34 @@ public class AccountsManager {
 		dbManager = new UserDatabaseManager(dataDirectory);
 	}
 	
+	// returns true if account with username exists
 	public boolean userExists(String username) {
 		return dbManager.userExists(username);
 	}
 	
+	// creates new file <username>.json with no data
 	public void createAccount(String username, String password) {
-		dbManager.addUser(username, password);
+		// adds username/password to database
+		dbManager.addUser(username, password); 
+		// creates new account with username
 		UserAccount account = new UserAccount(username, dataDirectory);
 	}
 	
+	// returns data (library) for given username
 	public LibraryModel getUserData(String username) {
+		// creates account object to access data in <username>.json
 		UserAccount account = new UserAccount(username, dataDirectory);
 		return account.getLibrary(); 
 	}
 	
 	public void updateAccount(String username, LibraryModel library) {
+		// creates account object to store new data in <username>.json
 		UserAccount account = new UserAccount(username, dataDirectory);
 		account.update(library);
 	}
 	
+	// returns true if password is correct for username
 	public boolean validatePassword(String username, String password) {
 		return dbManager.validatePassword(username, password);
-	}
-	
+	}	
 }
